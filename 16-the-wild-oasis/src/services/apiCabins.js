4e5520b7541a1c8e51cadfdaead99b496c0de127
Christer <christer.sayo@gmail.com>
2026-03-14 18:30:59 +0800

    The Wild Oasis project

---
+++ 16-the-wild-oasis/src/services/apiCabins.js
@@ -0,0 +1,37 @@
+import supabase from './supabase';
+
+export async function getCabins() {
+	const { data, error } = await supabase.from('cabins').select('*');
+
+	if (error) {
+		console.error(error);
+		throw new Error('Cabins could not be loaded.');
+	}
+
+	return data;
+}
+
+export async function createCabin(cabinData) {
+	const { data, error } = await supabase
+		.from('cabins')
+		.insert([cabinData])
+		.select();
+
+	if (error) {
+		console.error(error);
+		throw new Error('Cabin could not be created.');
+	}
+
+	return data;
+}
+
+export async function deleteCabin(id) {
+	const { data, error } = await supabase.from('cabins').delete().eq('id', id);
+
+	if (error) {
+		console.error(error);
+		throw new Error('Cabin could not be deleted.');
+	}
+
+	return data;
+}
---
+++ 16-the-wild-oasis/src/ui/Input.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+	border: 1px solid #ddd;
+	border-radius: 5px;
+	padding: 0.8rem 1.2rem;
+	box-shadow: var(--shadow-sm);
+`;
+
+export default Input;
