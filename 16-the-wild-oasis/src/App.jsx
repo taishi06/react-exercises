af74d77054758fa69008fd0a9f0bbff564906577
Christer <christer.sayo@gmail.com>
2026-03-23 17:10:32 +0800

    Wild Oasis - Dark and Light mode toggler

--- 16-the-wild-oasis/src/App.jsx
+++ 16-the-wild-oasis/src/App.jsx
@@ -15,6 +15,7 @@ import AppLayout from './ui/AppLayout';
 import Booking from './pages/Booking';
 import Checkin from './pages/Checkin';
 import ProtectedRoute from './ui/ProtectedRoute';
+import { DarkModeProvider } from './context/DarkModeContext';
 
 const queryClient = new QueryClient({
 	defaultOptions: {
@@ -28,58 +29,63 @@ const queryClient = new QueryClient({
 
 function App() {
 	return (
-		<QueryClientProvider client={queryClient}>
-			<GlobalStyles />
-			<BrowserRouter>
-				<Routes>
-					<Route
-						element={
-							<ProtectedRoute>
-								<AppLayout />
-							</ProtectedRoute>
-						}
-					>
-						{/* <Route element={<Navigate replace to="dashboard" />} /> */}
-						<Route path="dashboard" element={<Dashboard />} />
-						<Route path="bookings" element={<Bookings />} />
+		<DarkModeProvider>
+			<QueryClientProvider client={queryClient}>
+				<GlobalStyles />
+				<BrowserRouter>
+					<Routes>
 						<Route
-							path="bookings/:bookingId"
-							element={<Booking />}
-						/>
+							element={
+								<ProtectedRoute>
+									<AppLayout />
+								</ProtectedRoute>
+							}
+						>
+							{/* <Route element={<Navigate replace to="dashboard" />} /> */}
+							<Route path="dashboard" element={<Dashboard />} />
+							<Route path="bookings" element={<Bookings />} />
+							<Route
+								path="bookings/:bookingId"
+								element={<Booking />}
+							/>
+							<Route
+								path="checkin/:bookingId"
+								element={<Checkin />}
+							/>
+							<Route path="cabins" element={<Cabins />} />
+							<Route path="users" element={<Users />} />
+							<Route path="settings" element={<Settings />} />
+							<Route path="account" element={<Account />} />
+						</Route>
+
 						<Route
-							path="checkin/:bookingId"
-							element={<Checkin />}
+							index
+							element={<Navigate replace to="login" />}
 						/>
-						<Route path="cabins" element={<Cabins />} />
-						<Route path="users" element={<Users />} />
-						<Route path="settings" element={<Settings />} />
-						<Route path="account" element={<Account />} />
-					</Route>
+						<Route path="login" element={<Login />} />
+						<Route path="*" element={<PageNotFound />} />
+					</Routes>
+				</BrowserRouter>
 
-					<Route index element={<Navigate replace to="login" />} />
-					<Route path="login" element={<Login />} />
-					<Route path="*" element={<PageNotFound />} />
-				</Routes>
-			</BrowserRouter>
-
-			<Toaster
-				position="top-center"
-				gutter={12}
-				containerStyle={{ margin: '8px' }}
-				toastOptions={{
-					success: { duration: 3000 },
-					error: { duration: 5000 },
-					style: {
-						fontSize: '16px',
-						maxWidth: '500px',
-						padding: '16px 24px',
-						backgroundColor: 'var(--color-grey-0)',
-						color: 'var(--color-grey-700)',
-					},
-				}}
-			/>
-			<ReactQueryDevtools initialIsOpen={false} />
-		</QueryClientProvider>
+				<Toaster
+					position="top-center"
+					gutter={12}
+					containerStyle={{ margin: '8px' }}
+					toastOptions={{
+						success: { duration: 3000 },
+						error: { duration: 5000 },
+						style: {
+							fontSize: '16px',
+							maxWidth: '500px',
+							padding: '16px 24px',
+							backgroundColor: 'var(--color-grey-0)',
+							color: 'var(--color-grey-700)',
+						},
+					}}
+				/>
+				<ReactQueryDevtools initialIsOpen={false} />
+			</QueryClientProvider>
+		</DarkModeProvider>
 	);
 }
 
