137b99c8f6ee9eec840c81140da4ab381eac6a61
Christer <christer.sayo@gmail.com>
2026-03-26 17:52:02 +0800

    The wild oasis website - Next.JS built

---
+++ 18-the-wild-oasis-website/app/account/page.js
@@ -0,0 +1,13 @@
+// page conventions
+export const metadata = {
+	// <head><title> tag
+	title: 'Account',
+};
+
+export default function Page() {
+	return (
+		<h2 className="font-semibold text-2xl text-accent-400 mb-7">
+			Welcome, Chris
+		</h2>
+	);
+}
